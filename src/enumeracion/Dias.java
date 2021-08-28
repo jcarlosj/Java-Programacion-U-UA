14e62e3d80791067b4958585a9320f85bf4c6a1b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-08-28 17:10:42 -0500

    [+] Enumeracion: Define enumeracion e implementa en un switch case

---
+++ src/enumeracion/Dias.java
@@ -0,0 +1,14 @@
+package enumeracion;
+
+// Enumeracion
+public enum Dias {
+    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+}
+
+/* Enumeraciones:
+    - Por defecto los elementos de una enumeración son:
+      publicos (public), estáticos (static) y constantes (final).
+    - Nos permite crear facilmente listados de elementos con estas 
+      características
+    - Cada elemento se puede tratar como un valor String
+*/
---
+++ src/enumeracion/StartEnumeracion.java
@@ -0,0 +1,39 @@
+package enumeracion;
+
+public class StartEnumeracion {
+    public static void main( String[] args ) throws Exception {
+        System.out.println( Dias .DOMINGO );
+        indicarDiaSemana( Dias .DOMINGO );
+    }
+
+    private static void indicarDiaSemana( Dias dias ) {
+
+        // Switch es capar de hacer uso de una enumeracion
+        switch( dias ) {
+            case LUNES:
+                System.out.println( "Primer dia de la semana" );
+                break;
+            case MARTES:
+                System.out.println( "Segundo dia de la semana" );
+                break;
+            case MIERCOLES:
+                System.out.println( "Tercer dia de la semana" );
+                break;
+            case JUEVES:
+                System.out.println( "Cuarto dia de la semana" );
+                break;
+            case VIERNES:
+                System.out.println( "Quinto dia de la semana" );
+                break;
+            case SABADO:
+                System.out.println( "Sexto dia de la semana" );
+                break;
+            case DOMINGO:
+                System.out.println( "Septimo dia de la semana" );
+                break;
+            default:
+                System.out.println( "Dia de la semana desconocido" );
+                break;
+        }
+    }
+}
